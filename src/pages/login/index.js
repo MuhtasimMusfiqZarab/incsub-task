531b2d5f19190c7728b1677495acef5f7ae8096a
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-09-13 01:28:46 +0600

    structure change

---
+++ src/pages/login/index.js
@@ -0,0 +1,11 @@
+import  styles from './style.module.scss';
+
+function Login() {
+  return (
+    <div className={styles.container}>
+      <p>Hello world!</p>
+    </div>
+  );
+}
+
+export default Login;
