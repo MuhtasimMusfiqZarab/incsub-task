edccea20d07113af5410f3a5a1abf5dec6baddd8
Muhtasim Musfiq Zarab <musfiqzarab@iut-dhaka.edu>
2021-09-13 23:27:51 +0600

    alignment fix for two sections

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,15 @@
 import  styles from './style.module.scss';
-import {SigninIcon} from "../../components/_icons/signinIcon";
+// import {SigninIcon} from "../../components/_icons/signinIcon";
 import SignupForm from "../../components/signup-form";
 
 function Login() {
   return (
     <div className={styles.container}>
-        
         <div className={styles.signupContainer}>
           <SignupForm />
+        </div>
+        <div className={styles.brand}>
+        <SignupForm />
         </div>
       </div>
   );
